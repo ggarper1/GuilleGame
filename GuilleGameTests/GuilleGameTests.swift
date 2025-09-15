4f1b218c7f7b66b75456ecca2cb6480a812be915
ggarper1 <137006154+ggarper1@users.noreply.github.com>
2025-09-15 10:39:28 +0200

    Initial Commit

---
+++ GuilleGameTests/GuilleGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuilleGameTests.swift
+//  GuilleGameTests
+//
+//  Created by Guillermo Garcia Perez on 15/9/25.
+//
+
+import Testing
+@testable import GuilleGame
+
+struct GuilleGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
